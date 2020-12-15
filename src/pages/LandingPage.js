0eda35ac5b21a46fb32df7c1173dbabba9f376c2
Rizky Ian Indiarto <rizkyian78@gmail.com>
2020-12-15 23:36:58 +0700

    initial commit

--- src/pages/LandingPage.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react'
-
-import moduleName from 'elements/Button/index'
-export default class LandingPage extends Component {
-  asdarender() {
-    return <div></div>
-  }
-}
---
+++ src/parts/IconText.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from 'elements/Button/index'
+
+export default function IconText(props) {
+  return (
+    <Button className="brand-text-icon" href="" type="link">
+      Stay<span className="text-gray-900">cation.</span>
+    </Button>
+  )
+}
